077064359ae9a4e6133ff735b875c5396069e048
webala <webdspam@gmail.com>
2022-09-04 12:53:02 +0300

    ADD: models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Package(models.Model):
+    pass
+
+class Shoot(models.Model):
+    pass
+
+class Client(models.Model):
+    pass
+

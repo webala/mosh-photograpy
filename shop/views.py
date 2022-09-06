694531d2d682ea0da00ba7094ed362be511e7932
webala <webdspam@gmail.com>
2022-09-06 14:44:41 +0300

    REFACTOR: packages section

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,15 @@
-from .models import PackageCateagory
+from .models import Package, PackageCateagory
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    packages = PackageCateagory.objects.all()
+    package_categories = PackageCateagory.objects.all()
+    packages = Package.objects.all()
 
     context = {
-       'packages': packages 
+       'packages': packages,
+       'package_categories': package_categories
     }
-    
+
     return render(request, 'home.html', context)

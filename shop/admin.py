c4b5a01f3072da1b657812999525e17b9dc2d4f3
webala <webdspam@gmail.com>
2022-09-15 14:54:25 +0100

    ADD: pachages model

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from shop.models import GalleryImage
+from shop.models import GalleryImage, Package
 
 # Register your models here.
 admin.site.register(GalleryImage)
+admin.site.register(Package)
--- shop/models.py
+++ shop/models.py
@@ -2,18 +2,32 @@ from django.db import models
 
 
 # Create your models here.
-class Wedding(models.Model):
-    pass
+class Package(models.Model):
+    nature_choice = [
+        ('PHOTOGRAHY', 'PHOTOGRAHY'),
+        ('VIDEOGRAPHY', 'VIDEOGRAPHY'),
+    ]
 
-class Ruracio(models.Model):
-    pass
+    category_choices = [
+        ('BRONZE', 'BRONZE'),
+        ('SILVER', 'SILVER'),
+        ('GOLD', 'GOLD') 
+    ]
 
-class Portrait(models.Model):
-    pass
+    type_choices = [
+        ('WEDDING', 'WEDDING'),
+        ('PORTRAIT', 'PORTRAIT'),
+        ('RURACIO', 'RURACIO')
+    ]
 
-class WeddingCategories(models.Model):
-    pass
+    type = models.CharField(max_length=10, choices=type_choices, default='WEDDING')
+    category = models.CharField(max_length=10, choices=category_choices, null=True, blank=True)
+    nature = models.CharField(max_length=15, choices=nature_choice, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
+
+    def __str__(self) -> str:
+        return self.type + ' ' + self.nature + ' ' +  self.category
     
         
 class GalleryImage(models.Model):

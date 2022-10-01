e3dfd6a36aaecc2462c4244cacc19a200915372e
webala <webdspam@gmail.com>
2022-10-01 14:49:02 +0300

    ADD: shoots list

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('transactions', TransactionsList.as_view(), name='transactions-list'),
-    path('messages', MessagesList.as_view(), name='messages-list')
+    path('messages', MessagesList.as_view(), name='messages-list'),
+     path('shoots', ShootsList.as_view(), name='shoots-list'),
 ]

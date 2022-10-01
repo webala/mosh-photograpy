2040cd00056565430b1ee536cce0af4e55c63b03
webala <webdspam@gmail.com>
2022-10-01 14:55:23 +0300

    ADD: paginator template

--- dashboard/views.py
+++ dashboard/views.py
@@ -22,13 +22,16 @@ class TransactionsList(ListView):
     model = Transaction
     template_name: str = 'transactions.html'
     context_object_name: str = 'transactions'
+    paginate_by: int = 10
 
 class MessagesList(ListView):
     model = Message
     template_name: str = 'messages.html'
     context_object_name: str = 'client_messages'
+    paginate_by: int = 10
 
 class ShootsList(ListView):
     model = Shoot
     template_name: str = 'shoots.html'
     context_object_name: str = 'shoots'
+    paginate_by: int = 10
